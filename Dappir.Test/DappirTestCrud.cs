bd5c962f4d00f6b82f973eded1deeee894f7fffb
Anderson Fernandes <aasf_86@hotmail.com>
2017-10-26 22:25:17 -0200

    escrevendo testes

---
+++ Dappir.Test/DappirTestCrud.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dappir.Test
+{
+    [TestClass]
+    public class DappirTestCrud
+    {
+        [TestMethod]
+        public void Insert_entity()
+        {
+            Assert.IsTrue(1 > 0);
+        }
+    }
+}
